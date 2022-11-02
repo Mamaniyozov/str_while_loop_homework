a7582e54242b1ec4493f8f2000b3605079c7a8a6
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-11-02 21:45:29 +0500

    2-m

--- While02.py
+++ While02.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    k=0
+    i=0
+    while i<len(s):
+        if s[i].isalpha():
+            k+=1
+        i+=1
+
+        
+    return k
+print(main('salom80938'))
