2d8adcac042b8d205995d20d53d900622d1d727b
Mamaniyozov <115487574+Mamaniyozov@users.noreply.github.com>
2023-07-11 22:15:30 +0500

    Update While06.py
    6- m

--- While06.py
+++ While06.py
@@ -7,3 +7,14 @@ def main(s):
     Returns:
         int: return answer
     """
+    h=[a,e,i,o,u]
+    i=0 
+    k=0 
+    while i<len(s):
+        while k<len(h)
+        if s[i]==h[k]:
+            s[i].pop
+            k+=1
+        i+=1
+    return len(s)
+print (main("asdoqiwendaushd"))
